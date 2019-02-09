fa27d24a460124b1a139cd41868e2e88068b1894
Nathanael <nat11public@gmail.com>
2019-02-10 03:41:06 +1100

    red light green light wanderbot complete

--- src/red_light_green_light.py
+++ src/red_light_green_light.py
@@ -2,6 +2,7 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+#print "Starting package"
 cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
 rospy.init_node('red_light_green_light')
 
@@ -10,15 +11,20 @@ green_light_twist = Twist()
 green_light_twist.linear.x = 0.5
 
 driving_forward = False
-light_change_time = rospy.Time.now()
+light_change_time = rospy.get_rostime()
 rate  = rospy.Rate(10)
 
 while not rospy.is_shutdown():
+#	print light_change_time
+#	print rospy.get_rostime()
 	if driving_forward:
+#		print "pub green light"
 		cmd_vel_pub.publish(green_light_twist)
 	else:
+#		print "pub red light"
 		cmd_vel_pub.publish(red_light_twist)
-	if light_change_time > rospy.Time.now()
+	if light_change_time < rospy.get_rostime():
+		print "flipping driing_forward"
 		driving_forward = not driving_forward
-		light_change_time = rospy.Time.now() + rospy.Duration(3)
+		light_change_time = rospy.get_rostime() + rospy.Duration(3)
 	rate.sleep()
